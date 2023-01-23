eb051af2c1e3e2ea51899f0922b3b510812eba1f
ralpho79 <101933657+ralpho79@users.noreply.github.com>
2023-01-23 14:51:32 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,32 @@
-from flask import Flask
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        category = request.form["category"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(category),
+            temperature=0.5,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(category):
+    return """Recommend the best 3 {} movies to watch:""".format(
+        category.capitalize()
+    )
